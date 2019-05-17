270f73519bda54286c4507e1bf0242d5988644b0
Reza Khosroshahi <rzkhosroshahi@gmail.com>
2019-05-18 01:23:03 +0430

    add selection sort implementation

--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,7 +1,12 @@
 import { takeGreatest } from "./utils/takeGreatest";
+import { swap } from "./utils/swap";
 
 export const selectionSort = arr => {
-  arr.map((item, id) => {});
-  // for (let i)
+  for (let i = arr.length; i >= 0; i--) {
+    const newArr = arr.slice(0, i);
+    const greate = takeGreatest(newArr);
+    const finIndex = arr.findIndex(item => item === greate);
+    swap(arr, newArr.length - 1, finIndex);
+  }
   return arr;
 };
--- src/test/selectionSort.test.js
+++ src/test/selectionSort.test.js
@@ -1,5 +1,6 @@
 import { selectionSort } from "../selectionSort";
 
 test("selectionSort ", () => {
-  expect(selectionSort([5, 1, 2, 7, 3, 6])).toEqual([1, 2, 3, 5, 6, 7]);
+  const arr = [1, 9, 8, 7, 6, 5, 4, 3, 2];
+  expect(selectionSort(arr)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
 });
