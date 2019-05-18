eee3bd268c9d9839c8fd65245bcbc74b0dd54f96
Reza Khosroshahi <rzkhosroshahi@gmail.com>
2019-05-18 15:33:40 +0430

    change selection sort

--- src/test/selectionSort.test.js
+++ src/test/selectionSort.test.js
@@ -1,6 +1,6 @@
 import { selectionSort } from "../selectionSort";
 
 test("selectionSort ", () => {
-  const arr = [0, 1, 9, 8, 7, 6, 5, 4, 3, 2];
-  expect(selectionSort(arr)).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  const arr = [1, 9, 8, 7, 6, 5, 4, 3, 2];
+  expect(selectionSort(arr)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
 });
